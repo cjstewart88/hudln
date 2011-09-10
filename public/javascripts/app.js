34c0f1c13e39b180bb052b34206188b520fd3f5a
cjstewart88 <cjstewart88@gmail.com>
2011-09-10 12:43:54 -0300

    basic syntax changes

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,19 +1,17 @@
 (function() {
 
-  var socket = new io.connect('http://localhost:3000');
-	//var socket = new io.Socket("http://127.0.0.1/", {port: "3000", rememberTransport: false});
+	var socket = new io.connect('http://localhost:3000');
 
 	socket.on('connect', function() {
 		socket.emit('initial', { event: 'initial' });
 	});
 
 	socket.on('initial', function (message) {
-    console.log(message);
 		switch(message.event) {
 			case 'initial':
 				console.log(message.data);
 			break;
-			
+
 			case 'message':
 				console.log(message.data);
 			break;
@@ -22,22 +20,18 @@
 
 	var SocketProxy = function(){}
 	SocketProxy.sendMessage = function(mySessionId) {
-			socket.send({
-				event: 'message',
-				data: {
-					boo: "awesome"
-				}
-			});
-		};
-
-	console.log(SocketProxy);
+		socket.send({
+			event: 'message',
+			data: {
+				boo: "awesome"
+			}
+		});
+	};
 	
-  $(document).ready(function(){
-    $(".sendMsg").click(function(){
-      console.log(SocketProxy);
-      SocketProxy.sendMessage();
-      console.log("test");
-    });  
-  })
+	$(document).ready(function(){
+		$(".sendMsg").click(function(){
+			SocketProxy.sendMessage();
+		});  
+	})
   
 })();
