c7d334c59ea7b726ee51d86c40e8d70fcf5cc96d
Cody Stewart <cjstewart88@gmail.com>
2011-09-10 14:57:41 -0400

    cleaning some shit up a little and working on sending data back to the user.

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
+var sys = require('sys');
 var express = require('express');
 var io = require('socket.io');
 
-var app = module.exports = express.createServer();
+var app = express.createServer();
 
-// Configuration
 app.configure(function() {
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
   app.set('view engine', 'html');
+  app.set('views', __dirname + '/views');
   app.set("view options", {layout: false});
-  
   app.register('.html', {
     compile: function(str, options){
       return function(locals){
@@ -17,30 +15,20 @@ app.configure(function() {
       };
     }
   });
-  
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
   app.use(express.static(__dirname + '/public'));
 });
 
-app.configure('development', function() {
-  app.set('address', 'localhost');
-  app.use(express.errorHandler({
-    dumpExceptions: true,
-    showStack: true
-  }));
-});
-
-// Routes
+// routes
 app.get('/', function(req, res) {
   res.render('index');
 });
 
-if (!module.parent) {
-  app.listen(app.settings.port);
-  console.log("Server listening on port %d", app.settings.port);
-}
+app.listen(8080);
 
-// Start my Socket.io app and pass in the socket
-require('./socketapp').start(io.listen(app));
+var socket = io.listen(app);
+
+socket.on('connection', function (client) {
+    client.on('message', function (message) {
+        socket.broadcast(message);
+    });
+});
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,36 +1,20 @@
 (function() {
 
-	var socket = new io.connect('http://localhost:3000');
-
-	socket.on('connect', function() {
-		socket.emit('initial', { event: 'initial' });
-	});
-
-	socket.on('initial', function (message) {
-		switch(message.event) {
-			case 'initial':
-				console.log(message.data);
-			break;
-
-			case 'message':
-				console.log(message.data);
-			break;
-		}
-	});
-
-	var SocketProxy = function(){}
-	SocketProxy.sendMessage = function(mySessionId) {
-		socket.send({
-			event: 'message',
-			data: {
-				boo: "awesome"
-			}
-		});
-	};
+	//var socket = new io.connect('http://localhost:3000');
+	var socket = io.connect('http://127.0.0.1:8080');
+  //var socket = new io.Socket(null, {rememberTransport: false, port: 8080});
 	
+	socket.on('connect', function() {
+		socket.emit('initial', { event: 'initial', message: 'boo!' });
+	});
+
+	socket.on('newMessage', function (newMessage) {
+	  $('body').append(newMessage.message)
+	});
+	  
 	$(document).ready(function(){
 		$(".sendMsg").click(function(){
-			SocketProxy.sendMessage();
+			socket.send('testing');
 		});  
 	})
   
