dac3d49cf5dceeee8c4e15cf821360b6142d4b53
Akil J. Brooks <akil.brooks@gmail.com>
2011-09-10 11:20:07 -0400

    Use io.connect; Fix sendMessage method

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,14 +1,14 @@
 (function() {
 
-  var socket = new io.Socket(config.address, {port: config.port, rememberTransport: false});
+  var socket = new io.connect('http://localhost:3000');
 	//var socket = new io.Socket("http://127.0.0.1/", {port: "3000", rememberTransport: false});
 
 	socket.on('connect', function() {
-		socket.send({ event: 'initial' });
+		socket.emit('initial', { event: 'initial' });
 	});
 
-	socket.on('message', function (message) {
-
+	socket.on('initial', function (message) {
+    console.log(message);
 		switch(message.event) {
 			case 'initial':
 				console.log(message.data);
@@ -20,11 +20,8 @@
 		}
 	});
 
-	socket.connect();
-
-	var SocketProxy = function() {
-
-		var sendMessage = function(mySessionId) {
+	var SocketProxy = function(){}
+	SocketProxy.sendMessage = function(mySessionId) {
 			socket.send({
 				event: 'message',
 				data: {
@@ -32,9 +29,7 @@
 				}
 			});
 		};
-		
-	}();
-	
+
 	console.log(SocketProxy);
 	
   $(document).ready(function(){
