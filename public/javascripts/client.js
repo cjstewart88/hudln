df0e23ef853dfdf00766132ef92d0bf6403a0f34
Cody Stewart <cjstewart88@gmail.com>
2011-09-11 15:50:25 -0400

    Now using html5's canvas o_o

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,55 +1,64 @@
 (function() {
-	var socket = io.connect(null);
+	var socket          = io.connect(null);
 	
-	var clients = {};
+	var clients         = {};
+	var realm           = document.getElementById("realm");
+	var realmDimensions = [0,0];
 	
-	var myClientId;
-	var myX = 0;
-	var myY = 0;
+	var myClientId      = 0;
+	var myX             = 0;
+	var myY             = 0;
 	
 	socket.on('connect', function () {
-	  socket.on('currentClients', function (data) {
-      myClientId = data.clientId;
-	    clients = data.clients;
-      $.each(clients, function(client) {
-        $('body').append('<div id="'+client+'" style="top: '+clients[client][1]+'; left: '+clients[client][0]+'"></div>');
-      });
+	  socket.on('init', function (data) {
+	    clients         = data.clients;
+	    realmDimensions = [data.realmDimensions[0], data.realmDimensions[1]];
+      myClientId      = data.clientId;
+	    
+	    drawRealm();
 	  });
 	  
-  	socket.on('newClient', function (data) {
-  	  $('body').append('<div id="'+data.clientId+'" style="top: '+0+'; left: '+0+'"></div>');
-  	});
-  	
-  	socket.on('updateClientPosition',function (data) {
-  	  if (!data.deleteClient) {
-  	    clients[data.clientId] = [data.clientId, data.clientX, data.clientY];
-  	    $('body').find('#'+data.clientId).css({ 'top': data.clientY, 'left': data.clientX });
-  	  }
-  	  else {
-  	    clients[data.clientId] = null;
-  	    $('body').find('#'+data.clientId).remove();
-  	  }
-  	});
+	  socket.on('drawRealm', function (data) {
+	    clients         = data.clients;
+	    realmDimensions = [data.realmDimensions[0], data.realmDimensions[1]];
+	    
+	    drawRealm();
+	  });
 	});
+  
+  function drawRealm () {
+    // clear the canvas for redrawing
+    realm.width   = realm.height = 0;
+    
+    // adjust the canvas aka realm to its actual live dimensions
+    realm.width   = realmDimensions[0];
+    realm.height  = realmDimensions[1];
+  
+    // plot the currently connected clients
+    $.each(clients, function (client) {
+      var realm_context = realm.getContext('2d');
+      realm_context.fillRect(clients[client][0], clients[client][1], 16, 16);
+    });
+  }
   
 	$(document).ready(function () { 
   	$(document).keydown(function (e) {
-  	  console.log("uhhh");
-  	    var oldX = myX;
-  	    var oldY = myY;
+  	    var numOfSteps  = 10;
+  	    var oldX        = myX;
+  	    var oldY        = myY;
   	    
         switch (e.keyCode) {
           case 37: // left
-            if (myX-1 >= 0) { myX--; }
+            if (myX-numOfSteps >= 0) { myX -= numOfSteps; }
             break;
           case 39: // right
-            myX++;
+            myX += numOfSteps;
             break;
           case 38: // up
-            if (myY-1 >= 0) { myY--; } 
+            if (myY-numOfSteps >= 0) { myY -= numOfSteps; } 
             break;
           case 40: // down
-            myY++;
+            myY += numOfSteps;
             break;
         }
         
