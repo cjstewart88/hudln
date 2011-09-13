70eba4a4b770b097067931dfff5e2ae5505d2db2
Cody Stewart <cjstewart88@gmail.com>
2011-09-12 21:44:30 -0400

    Changing the games ui up a bit. Fixed width and height. When a client moves, only data within 400px(X Axis) and 300px(Y Axis) of the client is returned.

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,112 +1,56 @@
 (function() {
 	var socket          = io.connect(null);
 	
-	var clients         = {};
+	var clientsView     = {};
 	var realm           = document.getElementById("realm");
-	var realmDimensions = [0, 0];
 	
 	var myClientId      = 0;
 	var myX             = 0;
 	var myY             = 0;
-	var oldX            = 0;
-	var oldY            = 0;
-	
-	var windowWidth     = 0;
-	var windowHeight    = 0;
-	var leftOffset      = 0;
-	var topOffset       = 0;
 	
 	socket.on('connect', function () {
 	  socket.on('init', function (data) {
-	    clients         = data.clients;
-	    realmDimensions = [data.realmDimensions[0], data.realmDimensions[1]];
+	    clientsView     = data.clientsView;
       myClientId      = data.clientId;
 	    
 	    drawRealm();
 	  });
 	  
 	  socket.on('drawRealm', function (data) {
-	    clients         = data.clients;
-	    realmDimensions = [data.realmDimensions[0], data.realmDimensions[1]];
+	    clientsView     = data.clientsView;
 	    
 	    drawRealm();
 	  });
 	});
   
-  function scrollRealm () {
-    if (myX > oldX || myY > oldY) { // scrolling if the user is moving right or down
-      if (myX >= leftOffset-50) { 
-        $('html, body').animate({ scrollLeft: leftOffset-400 }, 2000); 
-        leftOffset += windowWidth-400; 
-      }
-      if (myY >= topOffset-50) { 
-        $('html, body').animate({ scrollTop: topOffset-200 }, 2000); 
-        topOffset += windowHeight-200; 
-      }
-    }
-    else { // scrolling if th user is moving left or up
-    }
-  }
-  
   function drawRealm () {
     // clear the canvas for redrawing
     realm.width   = realm.height = 0;
-    
-    // adjust the canvas aka realm to its actual live dimensions
-    realm.width   = realmDimensions[0];
-    realm.height  = realmDimensions[1];
-    $('#realm').css({ 'margin-right': windowWidth+'px','margin-bottom': windowHeight+'px' });
+    realm.width   = 800; 
+    realm.height  = 600;
     
     // plot the currently connected clients
-    $.each(clients, function (client) {
+    $.each(clientsView, function (client) {
       var realm_context = realm.getContext('2d');
-      realm_context.fillRect(clients[client][0], clients[client][1], 16, 16);
-      
-      // addjust the clients view port if needed to keep them on the screen when moving
-      if (client == myClientId) {
-        scrollRealm();
-      }
+      realm_context.fillRect(clientsView[client][0], clientsView[client][1], 16, 16);
     });
   }
   
 	$(document).ready(function () { 
-	  windowWidth         = $(window).width();
-	  windowHeight        = $(window).height();
-	  leftOffset          = $(window).width();
-	  topOffset           = $(window).height();
-
 	  // Movement
   	$(document).keydown(function (event) {
-  	    var keyPressed  = event.keyCode;
-  	    var numOfSteps  = 10;
-  	    var explore     = 200;  /*  Number of pixels the user has to be before he/she 
-  	                                explores new parts of the realm. Also how much(in pixels)
-  	                                the user explores. */
-  	    oldX            = myX;
-  	    oldY            = myY;
-        
-        if (keyPressed >= 37 && keyPressed <= 40) event.preventDefault();
-        
-        if (keyPressed == 37) { // left
-          if (myX-numOfSteps >= 0) { myX -= numOfSteps; } 
-        }
-        else if (keyPressed == 39) { // right
-          if (myX+explore >= realmDimensions[0]) { realmDimensions[0] += explore; } 
-          myX += numOfSteps;
-        }
-        else if (keyPressed == 38) { // up
-          if (myY-numOfSteps >= 0) { myY -= numOfSteps; } 
-        }
-        else if (keyPressed == 40) { // down
-          if (myY+explore >= realmDimensions[1]) { realmDimensions[1] += explore; }
-          myY += numOfSteps;
-        }
-                
-        if (oldX != myX || oldY != myY) {
-          socket.emit('moveClient', { clientId: myClientId, clientX: myX, clientY: myY, realmDimensions: realmDimensions } );
-        }
-      });
+  	  var keyPressed = event.keyCode;
+  	     
+      if      (keyPressed == 37)  myX -= 10; //left
+      else if (keyPressed == 39)  myX += 10; //right
+      else if (keyPressed == 38)  myY -= 10; //up
+      else if (keyPressed == 40)  myY += 10; //down
+                    
+      // if the key pressed is any of the arrow keys the user is moving, send the new position    
+      if (keyPressed >= 37 && keyPressed <= 40) { 
+        event.preventDefault();
+        socket.emit('moveClient', { clientX: myX, clientY: myY } );
+      }
+	  });
 	});
-	
-	$(document).mousewheel(function(event) { event.preventDefault(); });
 })();
