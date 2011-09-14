4e85a2c6b38143f06210469b5876703cebe54d52
Cody Stewart <cjstewart88@gmail.com>
2011-09-14 10:49:04 -0400

    Working with Jason on positioning of player

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -30,7 +30,24 @@
     // draw all the data in the clients range
     $.each(clientsView, function (client) {
       var realm_context = realm.getContext('2d');
-      realm_context.fillRect(clientsView[client][0], clientsView[client][1], 16, 16);
+      var plotX = 0;
+      var plotY = 0;
+      if (client == myClientId) {
+        plotX = 400;
+        plotY = 300;
+      }
+      else {
+        // myX = 2000 
+        // myY = 3000
+        // some random player's X = 1500
+        // some random player's  Y = 2500
+        plotX = myX-clientsView[client][0]; // 500
+        plotY = myY-clientsView[client][1]; // 500
+        
+        plotX -= 400;
+        plotY -= 300;
+      }
+      realm_context.fillRect(plotX, plotY, 10, 10);
     });
   }
   
@@ -39,10 +56,10 @@
   	$(document).keydown(function (event) {
   	  var keyPressed = event.keyCode;
   	     
-      if      (keyPressed == 37)  myX -= 10; //left
-      else if (keyPressed == 39)  myX += 10; //right
-      else if (keyPressed == 38)  myY -= 10; //up
-      else if (keyPressed == 40)  myY += 10; //down
+      if      (keyPressed == 37)  { if (myX-10 >= 0)      { myX -= 10; } } //left
+      else if (keyPressed == 39)  { if (myX+10 < 4000)    { myX += 10; } } //right
+      else if (keyPressed == 38)  { if (myY-10 >= 0)      { myY -= 10; } } //up
+      else if (keyPressed == 40)  { if (myY+10 < 3000)    { myY += 10; } } //down
                     
       // if the key pressed is any of the arrow keys the user is 
       // moving, send the new position and prevent scrolling with the arrow keys   
