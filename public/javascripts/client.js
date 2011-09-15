f3aba64f28aaf888c3c3b443bd1e13c11c898af1
Cody Stewart <cjstewart88@gmail.com>
2011-09-14 23:52:11 -0400

    realm walls and movement

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -2,56 +2,71 @@
 	var socket          = io.connect(null);
 	
 	var realm           = document.getElementById("realm");
+  var realm_context   = realm.getContext('2d');
+	var clients         = {};
 	
 	var myClientId      = 0;
 	var myX             = 0;
 	var myY             = 0;
 	
 	socket.on('connect', function () {
-	  socket.on('init', function (data) {
-      myClientId = data.clientId;
-	    drawRealm(data.clientsView);
+	  socket.on('init',function(data){
+	    myClientId  = data.clientId;
+  	  clients     = data.clients;
+	    drawRealm();
 	  });
 	  
 	  socket.on('drawRealm', function (data) {
-	    drawRealm(data.clientsView);
+	    clients     = data.clients;
+	    drawRealm();
 	  });
 	});
   
-  function drawRealm (clientsView) {
+  function drawRealm () {
     // clear the canvas for redrawing
     realm.width   = realm.height = 0;
     realm.width   = 800; 
     realm.height  = 600;
-    
-    // all the data the client can see
-    var clientsView = clientsView;
-    
+        
     // draw all the data in the clients range
-    $.each(clientsView, function (client) {
-      var realm_context = realm.getContext('2d');
+    $.each(clients, function (client) {
       var plotX = 0;
       var plotY = 0;
+      
+      realm_context.fillStyle = 'rgb(0,0,0)';
+      // draw realm boundaries 
+      if (myY <= 300) {
+        realm_context.fillRect(0,0,800,300-myY);
+      }
+      if (myX <= 800) {
+        realm_context.fillRect(0,0,400-myX,600);
+      }
+      if (myY >= 2500) {
+        realm_context.fillRect(0,3310-myY,800,300); 
+      }
+      if (myX >= 3500) {
+        realm_context.fillRect(4410-myX,0,400,600);
+      }
+      
       if (client == myClientId) {
-        realm_context.fillStyle = 'rgb(0,0,0)';
+        realm_context.fillStyle = 'rgb(255,0,0)';
         plotX = 400;
         plotY = 300;
       }
       else {
-        realm_context.fillStyle = 'rgb(255,0,0)';
+        realm_context.fillStyle = 'rgb(255,255,0)';
         // myX = 2000 
         // myY = 3000
         // some random player's X = 1500
         // some random player's  Y = 2500
-        plotX = myX-clientsView[client][0]; // 500
-        plotY = myY-clientsView[client][1]; // 500
+        plotX = myX-clients[client][0]; // 500
+        plotY = myY-clients[client][1]; // 500
         
         plotX = (plotX-400)*-1;
         plotY = (plotY-300)*-1;
       }
+      
       realm_context.fillRect(plotX, plotY, 10, 10);
-    
-      // console.log("plotX: " + plotX + " plotY: "+ plotY);
     });
   }
   
@@ -60,10 +75,10 @@
   	$(document).keydown(function (event) {
   	  var keyPressed = event.keyCode;
   	     
-      if      (keyPressed == 37)  { if (myX-10 >= 0)      { myX -= 10; } } //left
-      else if (keyPressed == 39)  { if (myX+10 < 4000)    { myX += 10; } } //right
-      else if (keyPressed == 38)  { if (myY-10 >= 0)      { myY -= 10; } } //up
-      else if (keyPressed == 40)  { if (myY+10 < 3000)    { myY += 10; } } //down
+      if      (keyPressed == 37)  { if (myX-100 >= 0)      { myX -= 100; } } //left
+      else if (keyPressed == 39)  { if (myX+100 <= 4000)    { myX += 100; } } //right
+      else if (keyPressed == 38)  { if (myY-100 >= 0)      { myY -= 100; } } //up
+      else if (keyPressed == 40)  { if (myY+100 <= 3000)    { myY += 100; } } //down
                     
       // if the key pressed is any of the arrow keys the user is 
       // moving, send the new position and prevent scrolling with the arrow keys   
