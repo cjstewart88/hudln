381397311c1ece9813417777e9bd08e98d5d89e7
Cody Stewart <cjstewart88@gmail.com>
2011-09-19 22:58:45 -0400

    added in a test sprite to simulate a character really moving, needs refactoring... may move this to server side

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -15,18 +15,37 @@
 	  });
 	  
 	  socket.on('clientConnected', function (data) {
-	    clients[data.clientId] = [data.x, data.y];
+	    clients[data.clientId] = [data.x, data.y, data.sx, data.sy];
 	    drawRealm();
 	  });
 	  	  
 	  socket.on('clientMoved', function (data) {
 	    clientThatMoved = data;
 	    
-	    if      (clientThatMoved.direction == "left")  clients[clientThatMoved.clientId][0] = clients[clientThatMoved.clientId][0]-100;
-      else if (clientThatMoved.direction == "right") clients[clientThatMoved.clientId][0] = clients[clientThatMoved.clientId][0]+100;
-      else if (clientThatMoved.direction == "up")    clients[clientThatMoved.clientId][1] = clients[clientThatMoved.clientId][1]-100;
-      else if (clientThatMoved.direction == "down")  clients[clientThatMoved.clientId][1] = clients[clientThatMoved.clientId][1]+100;
-      
+	    if      (clientThatMoved.direction == "left")  {
+	      clients[clientThatMoved.clientId][0] = clients[clientThatMoved.clientId][0]-10;
+        clients[clientThatMoved.clientId][3] = 96;
+        if (clients[clientThatMoved.clientId][2]+23 <= 46) clients[clientThatMoved.clientId][2] = clients[clientThatMoved.clientId][2]+24;
+        else clients[clientThatMoved.clientId][2] = 0;
+      }
+      else if (clientThatMoved.direction == "right") {
+        clients[clientThatMoved.clientId][0] = clients[clientThatMoved.clientId][0]+10;
+        clients[clientThatMoved.clientId][3] = 32;
+        if (clients[clientThatMoved.clientId][2]+23 <= 46) clients[clientThatMoved.clientId][2] = clients[clientThatMoved.clientId][2]+24;
+        else clients[clientThatMoved.clientId][2] = 0;
+      }
+      else if (clientThatMoved.direction == "up")    {
+        clients[clientThatMoved.clientId][1] = clients[clientThatMoved.clientId][1]-10;
+        clients[clientThatMoved.clientId][3] = 0;
+        if (clients[clientThatMoved.clientId][2]+23 <= 46) clients[clientThatMoved.clientId][2] = clients[clientThatMoved.clientId][2]+24;
+        else clients[clientThatMoved.clientId][2] = 0;
+      }
+      else if (clientThatMoved.direction == "down")  {
+        clients[clientThatMoved.clientId][1] = clients[clientThatMoved.clientId][1]+10;
+        clients[clientThatMoved.clientId][3] = 64;
+        if (clients[clientThatMoved.clientId][2]+23 <= 46) clients[clientThatMoved.clientId][2] = clients[clientThatMoved.clientId][2]+24;
+        else clients[clientThatMoved.clientId][2] = 0;
+      }
 	    drawRealm();
 	  });
 	  
@@ -44,40 +63,45 @@
     
     // clear the canvas for redrawing
     realm.width   = realm.height = 0;
-    realm.width   = 810; 
-    realm.height  = 610;
-        
-    // draw all the data in the clients range
-    $.each(clients, function (client) {
-      // draw realm boundaries
-      realm_context.fillStyle = 'rgb(0,0,0)';
-      if (myY <= 300)   realm_context.fillRect(0 ,0, 810, 300-myY);
-      if (myX <= 800)   realm_context.fillRect(0, 0, 400-myX, 610);
-      if (myY >= 2500)  realm_context.fillRect(0, 3310-myY, 810, 300); 
-      if (myX >= 3500)  realm_context.fillRect(4410-myX, 0, 400, 610);
+    realm.width   = 825; 
+    realm.height  = 632;
+    
+    var character = new Image();
+    character.src = "images/char3.png";
+    
+    character.onload = function() {
+      // draw all the data in the clients range
+      $.each(clients, function (client) {
+        // draw realm boundaries
+        realm_context.fillStyle = 'rgb(0,0,0)';
+        if (myY <= 300)   realm_context.fillRect(0 ,0, 825, 300-myY);
+        if (myX <= 800)   realm_context.fillRect(0, 0, 400-myX, 632);
+        if (myY >= 2500)  realm_context.fillRect(0, 3325-myY, 825, 300); 
+        if (myX >= 3500)  realm_context.fillRect(4432-myX, 0, 400, 632);
       
-      // draw clients
-      var plotX = 0;
-      var plotY = 0;
-      if (client == myClientId) {
-        realm_context.fillStyle = 'rgb(255,0,0)';
-        plotX = 400;
-        plotY = 300;
-      }
-      else {
-        realm_context.fillStyle = 'rgb(255,255,0)';
-        // myX = 2000  myY = 3000
-        // some random player's X = 1500
-        // some random player's  Y = 2500
-        plotX = myX-clients[client][0]; // 500
-        plotY = myY-clients[client][1]; // 500
+        // draw clients
+        var plotX = 0;
+        var plotY = 0;
+        if (client == myClientId) {
+          realm_context.fillStyle = 'rgb(255,0,0)';
+          plotX = 400;
+          plotY = 300;
+        }
+        else {
+          realm_context.fillStyle = 'rgb(255,255,0)';
+          // myX = 2000  myY = 3000
+          // some random player's X = 1500
+          // some random player's  Y = 2500
+          plotX = myX-clients[client][0]; // 500
+          plotY = myY-clients[client][1]; // 500
         
-        plotX = (plotX-400)*-1;
-        plotY = (plotY-300)*-1;
-      }
-      
-      realm_context.fillRect(plotX, plotY, 10, 10);
-    });
+          plotX = (plotX-400)*-1;
+          plotY = (plotY-300)*-1;
+        }
+        
+        realm_context.drawImage(character, clients[client][2], clients[client][3], 25, 32, plotX, plotY, 24, 32);
+      });
+    };
   }
   
 	$(document).ready(function () { 
