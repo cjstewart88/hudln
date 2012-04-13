e82a0544ab652fc56c0fa09f61242ec2575da30e
Cody Stewart <cjstewart88@gmail.com>
2012-04-13 02:22:51 -0400

    Started messing with randomly generating "items" and drawing them, available realm items are unique per client per connection. Also some cleanup.

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -6,16 +6,19 @@
 	var clients         = {};
 
 	var my_client_id    = 0;
-
+  var realm_items_available_to_client = null;
+  
 	socket.on('connect', function () {
 	  socket.on('init', function (data) {
 	    my_client_id  = data.client_id;
   	  clients       = data.init_client_list;
+  	  realm_items_available_to_client = data.realm_items_available_to_client;
+  	  
 	    draw_realm();
 	  });
 
 	  socket.on('client_connected', function (data) {
-	    clients[data.client_id] = [data.x, data.y, 23, 64];
+	    clients[data.client_id] = [data.x, data.y];
 	    draw_realm();
 	  });
 
@@ -30,25 +33,15 @@
 	});
 
   function save_move_locally (client_moving_id, direction, new_value) {
-    if (direction == "left") {
+    if      (direction == "left"  || direction == "right")  {
 			clients[client_moving_id][0] = new_value;
-			clients[client_moving_id][3] = 96;
     } 
-		else if (direction == "right") {
-			clients[client_moving_id][0] = new_value;
-			clients[client_moving_id][3] = 32;
-    } 
-		else if (direction == "up") {
+		else if (direction == "up"    || direction == "down")   {
 			clients[client_moving_id][1] = new_value;
-			clients[client_moving_id][3] = 0;
     } 
-		else if (direction == "down") {
-			clients[client_moving_id][1] = new_value;
-			clients[client_moving_id][3] = 64;
-    }
     
-    (clients[client_moving_id][2]+23 <= 46 ? clients[client_moving_id][2] += 24 : clients[client_moving_id][2] = 0);
-
+    console.log("x: "+clients[client_moving_id][0]+" y: "+clients[client_moving_id][1]);
+    
     draw_realm();
   }
 
@@ -62,61 +55,98 @@
 
     // clear the canvas for redrawing
     realm.width   = realm.height = 0;
-    realm.width   = 825; 
-    realm.height  = 632;
+    realm.width   = 800; 
+    realm.height  = 600;
 
 		// draw realm boundaries if players anywhere near them
 		realm_context.fillStyle = 'rgb(0,0,0)';
-		if (my_y <= 300)   realm_context.fillRect(0 ,0, 825, 300-my_y);
-		if (my_x <= 800)   realm_context.fillRect(0, 0, 400-my_x, 632);
-		if (my_y >= 2500)  realm_context.fillRect(0, 3325-my_y, 825, 300); 
-		if (my_x >= 3500)  realm_context.fillRect(4432-my_x, 0, 400, 632);
+		if      (my_y < 300)  realm_context.fillRect(0 , 0, 800, 300-my_y)
+		else if (my_y > 1600) realm_context.fillRect(0, 2320-my_y, 800, 300);
+
+		if      (my_x < 400)  realm_context.fillRect(0, 0, 400-my_x, 600);
+		else if (my_x > 1600) realm_context.fillRect(2420-my_x, 0, 400, 600);
 		
-    // draw all the data in the clients range
+    // draw all the clients
     $.each(clients, function (client) {
-      // draw clients
       if (client == my_client_id) {
+        realm_context.fillStyle = 'rgb(74, 23, 197)';
+        
         plot_x = 400;
         plot_y = 300;
       }
       else {
+        realm_context.fillStyle = 'rgb(25, 23, 36)';
+      
         plot_x = (my_x-clients[client][0]-400)*-1;
         plot_y = (my_y-clients[client][1]-300)*-1;
       }
       
-      realm_context.drawImage(character, clients[client][2], clients[client][3], 25, 32, plot_x, plot_y, 24, 32);
+  		realm_context.fillRect(plot_x, plot_y, 20, 20);
+    });
+    
+    // draw all the client specific realm items
+    $.each(realm_items_available_to_client, function (item) {
+      plot_x = (my_x-this.item_x-400)*-1;
+      plot_y = (my_y-this.item_y-300)*-1;
+      
+  		realm_context.fillStyle = 'rgb(234, 44, 70)';
+  		realm_context.fillRect(plot_x, plot_y, 10, 10);
     });
   }
   
-  // Client Movement
-	$(document).keydown(function (e) {  	 
-	  var direction = null;
-	  
-    if      (e.keyCode == 37) direction = "left";
-    else if (e.keyCode == 39) direction = "right";
-    else if (e.keyCode == 38) direction = "up";
-    else if (e.keyCode == 40) direction = "down";
-    
-    // if a direction was set lets attempt to move the character
-    if (direction) { 
-      event.preventDefault();
-      
-      // attempt to move locally up front to reduce lag and to prevent sending a request to the serve if the upfront 
-      // check does not legitly pass
-      var new_value = null;
-            
-      if      (direction == "left"  && clients[my_client_id][0]-10 >= 0     && clients[my_client_id][0] != 0)     new_value = clients[my_client_id][0]-10
-      else if (direction == "right" && clients[my_client_id][0]+10 <= 4000  && clients[my_client_id][0] != 4000)  new_value = clients[my_client_id][0]+10
-      else if (direction == "up"    && clients[my_client_id][1]-10 >= 0     && clients[my_client_id][1] != 0)     new_value = clients[my_client_id][1]-10
-      else if (direction == "down"  && clients[my_client_id][1]+10 <= 3000  && clients[my_client_id][1] != 3000)  new_value = clients[my_client_id][1]+10
-
-      if (new_value != null) {
-        save_move_locally(my_client_id, direction, new_value);
-
-        // send the request to the server to prevent cheating if the user some how moved beyond the 
-        // realm's boundaries the server will pull them back
-        socket.emit('request_to_move_client', { direction: direction, new_value: new_value } );
-      }
-    }
+  // Client Contols
+	$(document).keydown(function (e) { 
+	  var key = e.keyCode;
+	   
+	  if (key == 32) {
+	    pickup_item(event);
+	  }
+	  else if (key == 37 || key == 39 || key == 38 || key == 40) {
+	    client_movement(key);
+	  }
   });
+  
+  function pickup_item () {
+    event.preventDefault();
+    
+    my_x = clients[my_client_id][0];
+    my_y = clients[my_client_id][1];
+      
+    $.each(realm_items_available_to_client, function () {
+      var dx = this.item_x - my_x;
+      var dy = this.item_y - my_y;
+      
+      if (dx * dx + dy * dy <= 25 * 25) {
+        console.log("close to item");
+      }
+    });
+  }
+  
+  function client_movement (key) {
+    event.preventDefault();
+    
+    var direction = null;
+	  
+    if      (key == 37) direction = "left";
+    else if (key == 39) direction = "right";
+    else if (key == 38) direction = "up";
+    else if (key == 40) direction = "down";
+          
+    // attempt to move locally up front to reduce lag and to prevent sending a request to the serve if the upfront 
+    // check does not legitly pass
+    var new_value = null;
+          
+    if      (direction == "left"  && clients[my_client_id][0]-10 >= 0     && clients[my_client_id][0] != 0)     new_value = clients[my_client_id][0]-10
+    else if (direction == "right" && clients[my_client_id][0]+10 <= 2000  && clients[my_client_id][0] != 2000)  new_value = clients[my_client_id][0]+10
+    else if (direction == "up"    && clients[my_client_id][1]-10 >= 0     && clients[my_client_id][1] != 0)     new_value = clients[my_client_id][1]-10
+    else if (direction == "down"  && clients[my_client_id][1]+10 <= 2000  && clients[my_client_id][1] != 2000)  new_value = clients[my_client_id][1]+10
+
+    if (new_value != null) {
+      save_move_locally(my_client_id, direction, new_value);
+
+      // send the request to the server to prevent cheating if the user some how moved beyond the 
+      // realm's boundaries the server will pull them back
+      socket.emit('request_to_move_client', { direction: direction, new_value: new_value } );
+    }
+  }
 })();
