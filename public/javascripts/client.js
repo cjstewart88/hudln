1e3cec98f5f124a43133acd37b05f3f19094a985
Cody Stewart <cjstewart88@gmail.com>
2012-10-16 16:47:25 -0400

    Going to start working on this again, making it fully client side. Only reason for node is to eventually save user data server side if it goes in that direction.

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,55 +1,23 @@
 (function() {
-	var socket          = io.connect(null);
-
 	var realm           = document.getElementById("realm");
   var realm_context   = realm.getContext('2d');
-	var clients         = {};
 
-	var my_client_id    = 0;
-  var realm_items_available_to_client = null;
+  var me = {
+    x: 0,
+    y: 0
+  };
   
-	socket.on('connect', function () {
-	  socket.on('init', function (data) {
-	    my_client_id  = data.client_id;
-  	  clients       = data.init_client_list;
-  	  realm_items_available_to_client = data.realm_items_available_to_client;
-  	  
-	    draw_realm();
-	  });
-
-	  socket.on('client_connected', function (data) {
-	    clients[data.client_id] = [data.x, data.y];
-	    draw_realm();
-	  });
-
-	  socket.on('client_moved', function (data) {
-      save_move_locally(data.client_id, data.direction, data.new_value);
-	  });
-
-	  socket.on('client_disconnected', function (data) {
-	    delete clients[data.client_id];
-	    draw_realm();
-	  });
-	});
-
-  function save_move_locally (client_moving_id, direction, new_value) {
-    if      (direction == "left"  || direction == "right")  {
-			clients[client_moving_id][0] = new_value;
-    } 
-		else if (direction == "up"    || direction == "down")   {
-			clients[client_moving_id][1] = new_value;
-    }
-    
-    draw_realm();
-  }
+  var realm_items = generate_items();
+  
+  draw_realm();
 
   var character = new Image();
   character.src = "images/char3.png";
 
   function draw_realm () {
     // my cords
-    var my_x = clients[my_client_id][0];
-    var my_y = clients[my_client_id][1];
+    var my_x = me.x;
+    var my_y = me.y;
 
     // clear the canvas for redrawing
     realm.width   = realm.height = 0;
@@ -57,38 +25,24 @@
     realm.height  = 600;
 
 		// draw realm boundaries if players anywhere near them
-		realm_context.fillStyle = 'rgb(0,0,0)';
+		realm_context.fillStyle = 'rgb(0, 0, 0)';
 		if      (my_y < 300)  realm_context.fillRect(0 , 0, 800, 300-my_y)
 		else if (my_y > 1600) realm_context.fillRect(0, 2320-my_y, 800, 300);
 
 		if      (my_x < 400)  realm_context.fillRect(0, 0, 400-my_x, 600);
 		else if (my_x > 1600) realm_context.fillRect(2420-my_x, 0, 400, 600);
 		
-    // draw all the clients
-    $.each(clients, function (client) {
-      if (client == my_client_id) {
-        realm_context.fillStyle = 'rgb(74, 23, 197)';
-        
-        plot_x = 400;
-        plot_y = 300;
-      }
-      else {
-        realm_context.fillStyle = 'rgb(25, 23, 36)';
-      
-        plot_x = (my_x-clients[client][0]-400)*-1;
-        plot_y = (my_y-clients[client][1]-300)*-1;
-      }
-      
-  		realm_context.fillRect(plot_x, plot_y, 20, 20);
-    });
+    // draw yourself ... always in the center
+    realm_context.fillStyle = 'rgb(74, 23, 197)';
+		realm_context.fillRect(400, 300, 20, 20);
     
     // draw all the client specific realm items
-    $.each(realm_items_available_to_client, function (item) {
-      plot_x = (my_x-this.item_x-400)*-1;
-      plot_y = (my_y-this.item_y-300)*-1;
+    $.each(realm_items, function (item) {
+      item_x = (my_x-this.item_x-400)*-1;
+      item_y = (my_y-this.item_y-300)*-1;
       
   		realm_context.fillStyle = 'rgb(234, 44, 70)';
-  		realm_context.fillRect(plot_x, plot_y, 10, 10);
+  		realm_context.fillRect(item_x, item_y, 10, 10);
     });
   }
   
@@ -107,12 +61,9 @@
   function pickup_item () {
     event.preventDefault();
     
-    my_x = clients[my_client_id][0];
-    my_y = clients[my_client_id][1];
-      
-    $.each(realm_items_available_to_client, function () {
-      var dx    = this.item_x - (my_x + 5);
-      var dy    = this.item_y - (my_y + 5);
+    $.each(realm_items, function () {
+      var dx    = this.item_x - (me.x + 5);
+      var dy    = this.item_y - (me.y + 5);
       var dist  = Math.floor(Math.sqrt((dx * dx) + (dy * dy)));
       
       if (dist <= 20 || dist == 21) {
@@ -130,22 +81,29 @@
     else if (key == 39) direction = "right";
     else if (key == 38) direction = "up";
     else if (key == 40) direction = "down";
+    
+    var old_x = me.x;
+    var old_y = me.y;
           
-    // attempt to move locally up front to reduce lag and to prevent sending a request to the serve if the upfront 
-    // check does not legitly pass
-    var new_value = null;
-          
-    if      (direction == "left"  && clients[my_client_id][0]-10 >= 0     && clients[my_client_id][0] != 0)     new_value = clients[my_client_id][0]-10
-    else if (direction == "right" && clients[my_client_id][0]+10 <= 2000  && clients[my_client_id][0] != 2000)  new_value = clients[my_client_id][0]+10
-    else if (direction == "up"    && clients[my_client_id][1]-10 >= 0     && clients[my_client_id][1] != 0)     new_value = clients[my_client_id][1]-10
-    else if (direction == "down"  && clients[my_client_id][1]+10 <= 2000  && clients[my_client_id][1] != 2000)  new_value = clients[my_client_id][1]+10
+    if      (direction == "left"  && me.x-10 >= 0     && me.x != 0)     me.x-=10
+    else if (direction == "right" && me.x+10 <= 2000  && me.x != 2000)  me.x+=10
+    else if (direction == "up"    && me.y-10 >= 0     && me.y != 0)     me.y-=10
+    else if (direction == "down"  && me.y+10 <= 2000  && me.y != 2000)  me.y+=10
+    
+    if (old_x != me.x || old_y != me.y) draw_realm();
+  }
+  
+  function generate_items () {
+    var generated_items = [];
 
-    if (new_value != null) {
-      save_move_locally(my_client_id, direction, new_value);
-
-      // send the request to the server to prevent cheating if the user some how moved beyond the 
-      // realm's boundaries the server will pull them back
-      socket.emit('request_to_move_client', { direction: direction, new_value: new_value } );
+    for (var i = 0; i < 100; i++) {
+      generated_items.push({
+        item_id:  i,
+        item_x:   Math.floor(Math.random()*190)*10,
+        item_y:   Math.floor(Math.random()*190)*10
+      })
     }
+
+    return generated_items;
   }
 })();
--- server.js
+++ server.js
@@ -1,115 +1,25 @@
-var express = require('express');
-var io      = require('socket.io');
-var redis   = require('redis');
-
-var server  = express.createServer();
-
-server.configure(function() {
-  server.set('view engine', 'html');
-  server.set('views', __dirname + '/views');
-  server.set('view options', {layout: false});
-  server.register('.html', {
-    compile: function(str, options){
-      return function(locals){
-        return str;
-      };
-    }
-  });
-  server.use(express.static(__dirname + '/public'));
-});
-
-// routes
-server.get('/', function(req, res) {
-  res.render('index');
-});
-
-server.listen(3000);
-
-/*    REDIS SERVER    */
-var redis_port    = process.env.REDIS_PORT || 6379;
-var redis_host    = process.env.REDIS_HOST || '127.0.0.1';
-var redis_server  = redis.createClient(redis_port, redis_host);
-redis_server.auth(process.env.REDIS_KEY);
-
-/*    SOCKET.IO SHIT    */
-var io = io.listen(server);
-
-// handler for when a new client connects
-io.sockets.on('connection', function (client) {
-  // setup new clients stuff
-  var client_id = client['id'];
-  var realm_items_available_to_client = generate_items();
-  
-  // add the new client to the 'clients' set in redis
-  // one day this will check to see if the client that connected is already a client 
-  // and basically just "sign them in" and mark them connected
-  redis_server.sadd("clients", "client:"+client_id);
-  redis_server.hmset("client:"+client_id, "client_id", client_id, "status", "connected", "x", 0, "y", 0);
-
-  // get the list of currently connected clients and also let already connected 
-  // clients know about the new user
-  redis_server.smembers("clients", function (err, clients) {
-    var init_client_list = {};
-    var client_count = 0;
-
-    for (var thisclient_id in clients) {
-      redis_server.hgetall(clients[thisclient_id], function (err, client_data) {
-        if (client_data["status"] == "connected") {
-          init_client_list[client_data["client_id"]] = [parseInt(client_data["x"]), parseInt(client_data["y"])];
-        }
-
-        client_count++
-
-        // we only need to send our messages when all the currently connected clients are added to the 'init_client_list'
-        if (client_count == clients.length) {
-          // send the init action to get the newly connected client started
-          client.emit('init', { client_id: client_id, init_client_list: init_client_list, realm_items_available_to_client: realm_items_available_to_client });  
-          // let the already connected clients know about the newly connected client
-          client.broadcast.emit('client_connected', { client_id: client_id, x: 0, y: 0 }); 
-          // we dont need this anymore after its been sent
-          init_client_list = null; 
-        }
-      });
-    }
-  });
-
-  // clients requesting to move
-  client.on('request_to_move_client', function (data) {
-    var direction = data.direction;
-    var new_value = data.new_value;
+/*
+  Dependencies
+*/
+var express = require('express'),
+    ejs     = require('ejs'),
+    http    = require('http');
     
-    redis_server.hgetall("client:"+client_id, function (err, client) {
-      var x   = parseInt(client["x"]);
-      var y   = parseInt(client["y"]);
+/*
+  Initialize & Configure Express
+*/
 
-      if      (direction == "left"  && x-10 >= 0    )  redis_server.hset("client:"+client_id, "x", new_value, function() { client_moved(); });
-      else if (direction == "right" && x+10 <= 2000 )  redis_server.hset("client:"+client_id, "x", new_value, function() { client_moved(); });
-      else if (direction == "up"    && y-10 >= 0    )  redis_server.hset("client:"+client_id, "y", new_value, function() { client_moved(); });
-      else if (direction == "down"  && y+10 <= 2000 )  redis_server.hset("client:"+client_id, "y", new_value, function() { client_moved(); });
-    }); 
-    
-    function client_moved () {
-      client.broadcast.emit('client_moved', { client_id: client_id, direction: direction, new_value: new_value });
-    }
-  });
+var app = express();
 
-  // client disconnected
-  client.on('disconnect', function () {
-    redis_server.hset("client:"+client_id, "status", "disconnected");
-    client.broadcast.emit('client_disconnected', { client_id: client_id });
-  });
+app.configure(function () {
+  app.engine('html', ejs.renderFile);
+  app.set('view engine', 'html');
+  app.set('views', __dirname + '/views');
+  app.use(express.static(__dirname + '/public'));
+  app.get('/', function(req, res) { res.render('index'); });
 });
 
-function generate_items () {
-  var generated_items = [];
-  
-  for (var i = 0; i < 100; i++) {
-    generated_items.push({
-      item_id:  i,
-      item_x:   Math.floor(Math.random()*190)*10,
-      item_y:   Math.floor(Math.random()*190)*10
-    })
-  }
-  
-  return generated_items;
-}
+/*
+  Initialize the Server
+*/                       
+var server = http.createServer(app).listen(process.env.PORT || 3001);
