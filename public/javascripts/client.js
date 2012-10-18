40c735304d85d6906198ed4eea30b5db73a071f7
Cody Stewart <cjstewart88@gmail.com>
2012-10-18 10:53:36 -0400

    Added in the stuff to draw our sprite running and added in item collection basics.

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -3,17 +3,22 @@
   var realm_context   = realm.getContext('2d');
 
   var me = {
-    x: 0,
-    y: 0
+    x:          0,
+    y:          0,
+    sprite_sx:  23,
+    sprite_sy:  64,
+    resources:  0
   };
   
   var realm_items = generate_items();
   
-  draw_realm();
-
   var character = new Image();
   character.src = "images/char3.png";
-
+  
+  character.onload = function () {
+    draw_realm();
+  }
+  
   function draw_realm () {
     // my cords
     var my_x = me.x;
@@ -24,7 +29,7 @@
     realm.width   = 800; 
     realm.height  = 600;
 
-		// draw realm boundaries if players anywhere near them
+		// draw realm boundaries if player is anywhere near them
 		realm_context.fillStyle = 'rgb(0, 0, 0)';
 		if      (my_y < 300)  realm_context.fillRect(0 , 0, 800, 300-my_y)
 		else if (my_y > 1600) realm_context.fillRect(0, 2320-my_y, 800, 300);
@@ -33,8 +38,7 @@
 		else if (my_x > 1600) realm_context.fillRect(2420-my_x, 0, 400, 600);
 		
     // draw yourself ... always in the center
-    realm_context.fillStyle = 'rgb(74, 23, 197)';
-		realm_context.fillRect(400, 300, 20, 20);
+    realm_context.drawImage(character, me.sprite_sx, me.sprite_sy, 25, 32, 400, 300, 24, 32);
     
     // draw all the client specific realm items
     $.each(realm_items, function (item) {
@@ -44,6 +48,10 @@
   		realm_context.fillStyle = 'rgb(234, 44, 70)';
   		realm_context.fillRect(item_x, item_y, 10, 10);
     });
+  }
+  
+  function update_hud (to_update, value) {
+    $('#'+to_update).text(value);
   }
   
   // Client Contols
@@ -68,8 +76,15 @@
       
       if (dist <= 20 || dist == 21) {
         console.log("close to item");
+        delete this.item_id;
+        delete this.item_x;
+        delete this.item_y;
+        me.resources += 1;
+        update_hud('resources-collected', me.resources);
       }
     });
+    
+    draw_realm();
   }
   
   function client_movement (key) {
@@ -85,10 +100,29 @@
     var old_x = me.x;
     var old_y = me.y;
           
-    if      (direction == "left"  && me.x-10 >= 0     && me.x != 0)     me.x-=10
-    else if (direction == "right" && me.x+10 <= 2000  && me.x != 2000)  me.x+=10
-    else if (direction == "up"    && me.y-10 >= 0     && me.y != 0)     me.y-=10
-    else if (direction == "down"  && me.y+10 <= 2000  && me.y != 2000)  me.y+=10
+    if (direction == "left"  && me.x - 10 >= 0 && me.x != 0) {     
+      me.x -= 10
+      me.sprite_sy = 96;
+    }
+    else if (direction == "right" && me.x + 10 <= 2000 && me.x != 2000) {
+      me.x += 10
+      me.sprite_sy = 32;
+    }
+    else if (direction == "up" && me.y - 10 >= 0 && me.y != 0) {
+      me.y -= 10
+      me.sprite_sy = 0;
+    }
+    else if (direction == "down" && me.y + 10 <= 2000 && me.y != 2000) {
+      me.y += 10
+      me.sprite_sy = 64;
+    }
+    
+    if (me.sprite_sx + 23 <= 46) {
+      me.sprite_sx += 24
+    }
+    else {
+      me.sprite_sx = 0;
+    }
     
     if (old_x != me.x || old_y != me.y) draw_realm();
   }
